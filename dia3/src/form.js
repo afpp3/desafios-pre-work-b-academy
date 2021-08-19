a9af6dea80ce3fab4df0b2f78026fc542fffd684
Andre Felippe <afelipp3@gmail.com>
2021-08-19 14:35:58 -0300

    Desafio 01

---
+++ dia3/src/form.js
@@ -0,0 +1,17 @@
+const masks = {
+  nome(value) {
+    return value
+      .toLowerCase()
+      .replace(/(?:^|\s)\S/g, (upper) => upper.toUpperCase());
+  },
+};
+
+const nameInput = document.querySelector("[data-js='inputName']");
+
+nameInput.addEventListener(
+  "input",
+  (e) => {
+    e.target.value = masks.nome(text);
+  },
+  false
+);
