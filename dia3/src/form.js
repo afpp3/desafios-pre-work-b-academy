aa4f08a04a35475fa8995e26c6a212a8fb226639
Andre Felippe <afelipp3@gmail.com>
2021-08-20 00:45:18 -0300

    Desafio 02 finalizado

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -23,3 +23,66 @@ const verifyPreposition = (word) => {
     ? word.toLowerCase()
     : masks.nome(word);
 };
+
+const form = $("[data-js='form']");
+const select = document.createElement("select");
+const colors = [
+  {
+    cor: "vermelho",
+    hex: "#DD2C00",
+  },
+  {
+    cor: "preto",
+    hex: "#000000",
+  },
+  {
+    cor: "roxo",
+    hex: "#4A148C",
+  },
+  {
+    cor: "verde",
+    hex: "#1B5E20",
+  },
+  {
+    cor: "azul",
+    hex: "#01579B",
+  },
+];
+
+const colorsContainer = document.createElement("div");
+colorsContainer.style.display = "flex";
+colorsContainer.style.gap = "10px";
+
+const createOption = (color) => {
+  const option = document.createElement("option");
+  option.value = color.hex;
+  option.textContent = color.cor;
+  return option;
+};
+
+colors.forEach((color) => {
+  const option = createOption(color);
+  select.appendChild(option);
+});
+
+const createColorsDiv = (color) => {
+  const div = document.createElement("div");
+  div.style.height = "50px";
+  div.style.width = "50px";
+  div.style.borderRadius = "50%";
+  div.style.background = color;
+  return div;
+};
+
+select.addEventListener("change", (e) => {
+  colorsContainer.innerHTML = "";
+
+  Array.from(e.target.selectedOptions).forEach((option) => {
+    const div = createColorsDiv(option.value);
+    colorsContainer.appendChild(div);
+  });
+});
+
+select.setAttribute("multiple", "");
+form.appendChild(select);
+document.body.appendChild(colorsContainer);
